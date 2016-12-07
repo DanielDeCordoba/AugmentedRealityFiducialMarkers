17e04ae07ff63546451068d017a85bd594fe40cd
DanielDeCordoba <danidecordoba@gmail.com>
2016-12-06 21:25:00 -0600

    Folder cleanup

---
+++ SaltwashAR-master/scripts/objviewer.py
@@ -0,0 +1,119 @@
+# Basic OBJ file viewer. needs objloader from:
+#  http://www.pygame.org/wiki/OBJFileLoader
+# LMB + move: rotate
+# RMB + move: pan
+# Scroll wheel: zoom in/out
+import sys, pygame
+from pygame.locals import *
+from pygame.constants import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from webcam import Webcam
+import cv2
+from PIL import Image
+ 
+# IMPORT OBJECT LOADER
+from objloader import *
+ 
+pygame.init()
+viewport = (800,600)
+hx = viewport[0]/2
+hy = viewport[1]/2
+srf = pygame.display.set_mode(viewport, OPENGL | DOUBLEBUF)
+ 
+glLightfv(GL_LIGHT0, GL_POSITION,  (-40, 200, 100, 0.0))
+glLightfv(GL_LIGHT0, GL_AMBIENT, (0.2, 0.2, 0.2, 1.0))
+glLightfv(GL_LIGHT0, GL_DIFFUSE, (0.5, 0.5, 0.5, 1.0))
+glEnable(GL_LIGHT0)
+glEnable(GL_LIGHTING)
+glEnable(GL_COLOR_MATERIAL)
+glEnable(GL_DEPTH_TEST)
+glShadeModel(GL_SMOOTH)           # most obj files expect to be smooth-shaded
+ 
+# LOAD OBJECT AFTER PYGAME INIT
+obj = OBJ(sys.argv[1], swapyz=True)
+
+# webcam = Webcam()
+# webcam.start()
+
+clock = pygame.time.Clock()
+ 
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+width, height = viewport
+gluPerspective(90.0, width/float(height), 1, 100.0)
+glEnable(GL_DEPTH_TEST)
+glMatrixMode(GL_MODELVIEW)
+ 
+rx, ry = (0,0)
+tx, ty = (0,0)
+zpos = 5
+rotate = move = False
+while 1:
+    clock.tick(30)
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            sys.exit()
+        elif e.type == KEYDOWN and e.key == K_ESCAPE:
+            sys.exit()
+        elif e.type == MOUSEBUTTONDOWN:
+            if e.button == 4: zpos = max(1, zpos-1)
+            elif e.button == 5: zpos += 1
+            elif e.button == 1: rotate = True
+            elif e.button == 3: move = True
+        elif e.type == MOUSEBUTTONUP:
+            if e.button == 1: rotate = False
+            elif e.button == 3: move = False
+        elif e.type == MOUSEMOTION:
+            i, j = e.rel
+            if rotate:
+                rx += i
+                ry += j
+            if move:
+                tx += i
+                ty -= j
+
+    # # #render
+    # texture_background = glGenTextures(1)
+    #
+    # # draw background
+    # glBindTexture(GL_TEXTURE_2D, texture_background)
+    # glPushMatrix()
+    # glTranslatef(0.0, 0.0, -11.2)
+    #
+    # # convert image to OpenGL texture format
+    # image = webcam.get_current_frame()
+    # image = cv2.flip(image, 0)
+    # gl_image = Image.fromarray(image)
+    # ix = gl_image.size[0]
+    # iy = gl_image.size[1]
+    # gl_image = gl_image.tobytes("raw", "BGRX", 0, -1)
+    #
+    # glBindTexture(GL_TEXTURE_2D, texture_background)
+    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    # glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    # glTexImage2D(GL_TEXTURE_2D, 0, 3, ix, iy, 0, GL_RGBA, GL_UNSIGNED_BYTE, gl_image)
+
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    # # draw background
+    # glBegin(GL_QUADS)
+    # glTexCoord2f(0.0, 1.0);
+    # glVertex3f(-4.0, -3.0, 4.0)
+    # glTexCoord2f(1.0, 1.0);
+    # glVertex3f(4.0, -3.0, 4.0)
+    # glTexCoord2f(1.0, 0.0);
+    # glVertex3f(4.0, 3.0, 4.0)
+    # glTexCoord2f(0.0, 0.0);
+    # glVertex3f(-4.0, 3.0, 4.0)
+    # glEnd()
+    # glPopMatrix()
+
+    # RENDER OBJECT
+    glTranslate(tx/20., ty/20., - zpos)
+    glRotate(ry, 1, 0, 0)
+    glRotate(rx, 0, 1, 0)
+    glCallList(obj.gl_list)
+
+    pygame.display.flip()
